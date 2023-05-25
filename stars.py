f8bac3a985daf13eff14bb24deb45e3896fdc6f0
vinimaneirao <91675450+vinimaneirao@users.noreply.github.com>
2023-05-24 21:23:05 -0300

    Add files via upload

---
+++ stars.py
@@ -0,0 +1,46 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from bs4 import BeautifulSoup
+import time
+import pandas as pd
+
+urlsite = "https://en.wikipedia.org/wiki/List_of_brightest_stars_and_other_record_stars"
+pegarinfo = webdriver.Chrome("C:\\Users\\vinic\\Desktop\\Programacao\\PROJETOS (aula)\\coletadedadosweb\\chromedriver.exe")
+pegarinfo.get(urlsite)
+scarped_data = []
+
+def scrape():
+    soup = BeautifulSoup(pegarinfo.page_source, "html.parser")
+    bright_star_table = soup.find("table", attrs={"class", "wikitable"})
+    table_body = bright_star_table.find('tbody')
+    table_rows = table_body.find_all('tr')
+
+    for i in table_rows:
+        table_cols = i.find_all('td')
+        print(table_cols)
+
+        temp_list = []
+
+        for j in table_cols:
+            data = j.text.strip()
+            print(data)
+            temp_list.append(data)
+    
+    scarped_data.append(temp_list)
+
+stars_data = []
+
+for i in range(0, len(scarped_data)):
+
+    Star_names = scarped_data[i][1]
+    Distance = scarped_data[i][3]
+    Mass = scarped_data[i][5]
+    Radius = scarped_data[i][6]
+    Lum = scarped_data[i][7]
+
+    required_data = [Star_names, Distance, Mass, Radius, Lum]
+    stars_data.append(required_data)
+
+headers = ['Star_name', 'Distance', 'Mass', 'Radius', 'Luminosity']
+star_df_1 = pd.DataFrame(stars_data, columns = headers)
+star_df_1.to_csv('scraped_data.csv', index = True, index_label = "id")
